db7460b47ff119702da2da4c53291ff45e5a93b5
Gülzade Evni <glzd_75@hotmail.com>
2024-02-20 23:06:28 +0300

    add numpy basic file

---
+++ numpy-basic.py
@@ -0,0 +1,43 @@
+# #19.1
+import numpy as np
+
+# arr = np.array([1,2,3,4,5,6])
+# print(type(arr))
+
+# multi_arr = np.array([[1,2,3],[4,5,6]])
+# print(multi_arr)
+
+# arr2 = np.array([1,2,3,4,5,6,7,8,9])
+# matris = arr2.reshape(3,3)
+# print(matris)
+# print(matris.ndim)# kaç boyutlu olduğunu gösterir
+
+# 19.2
+
+# result = np.arange(1,10)
+# print(result)
+
+# result2 = np.linspace(0,100,5)
+# print(result2)
+
+# result3 = np.random.randint(0,10)
+# print(result3)
+
+# np_arr = np.arange(50)
+# np_multi = np_arr.reshape(5,10)
+# print(np_multi)
+# print(np_multi.sum(axis=1))# satırların toplamı
+# print(np_multi.sum(axis=0))# sütunların toplamı
+
+# rnd_numbers = np.random.randint(1,100,10)
+# print(rnd_numbers)
+# result = rnd_numbers.max()
+# print(result)
+
+# 19.3
+
+# numbers = np.array([[10,20,30],[40,50,60],[70,80,90]])
+# result = numbers[:,2]
+# result = numbers[:,0:2]
+
+# print(result)
